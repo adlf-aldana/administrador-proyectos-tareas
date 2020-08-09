c8248c4445ae8d04a2b29193eecc4945de33b267
adolfo <adlf.aldana@gmail.com>
2020-08-09 11:24:22 -0400

    componentes creados y rutas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Login from './components/auth/Login';
+import NuevaCuenta from './components/auth/NuevaCuenta';
+import Proyectos from './components/proyectos/Proyectos';
 
 function App() {
   return (
-    <h1>MERNTask</h1>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
+        <Route exact path="/proyectos" component={Proyectos} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
