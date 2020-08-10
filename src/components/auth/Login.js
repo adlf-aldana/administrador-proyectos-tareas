da8eb44df688b861dca49d5973833b8de2229925
adolfo <adlf.aldana@gmail.com>
2020-08-09 20:55:57 -0400

    formulario login

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,9 +1,48 @@
 import React from 'react';
 
 const Login = () => {
-    return ( 
-        <h1>Desde Login</h1>
+    
+    const onChange = () => {
+
+    }
+    
+    return (
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Iniciar Sesión</h1>
+                <form>
+
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Tu email"
+                            onChange={onChange} />
+                    </div>
+
+                    <div className="campo-form">
+                        <label htmlFor="password">Email</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Tu Password"
+                            onChange={onChange} />
+                    </div>
+
+                    <div className="campo-form">
+                        <input
+                            type="submit"
+                            className="btn btn-primario btn-block"
+                            value="Iniciar Sesion"
+                        />
+                    </div>
+                </form>
+            </div>
+        </div>
     );
 }
- 
+
 export default Login;
