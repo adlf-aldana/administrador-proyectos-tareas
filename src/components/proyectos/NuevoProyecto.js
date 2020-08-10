6d9bb57d5b2dad818e7ada026bc469b1c1afca3c
adolfo <adlf.aldana@gmail.com>
2020-08-09 21:49:59 -0400

    formulario nuevo proyecto

---
+++ src/components/proyectos/NuevoProyecto.js
@@ -0,0 +1,30 @@
+import React, { Fragment } from 'react';
+
+const NuevoProyecto = () => {
+    return (
+        <Fragment>
+            <button
+                type="button"
+                className="btn btn-block btn-primario"
+            >Nuevo Proyecto</button>
+
+            <form
+                className="formulario-nuevo-proyecto">
+                <input
+                    type="text"
+                    name="nombre"
+                    id="nombre"
+                    className="input-text"
+                    placeholder="Nombre Proyecto"
+                />
+
+                <input
+                    type="submit"
+                    className="btn btn-primario btn-block"
+                    value="Agregar Proyecto" />
+            </form>
+        </Fragment>
+    );
+}
+
+export default NuevoProyecto;
