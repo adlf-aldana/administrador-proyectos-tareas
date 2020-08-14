023b36aa20ae5edcbbb4054f6dc07ef43dfdbf44
adolfo <adlf.aldana@gmail.com>
2020-08-13 20:26:33 -0400

    insertando tareas al proyecto actual

--- src/components/tareas/FormTareas.js
+++ src/components/tareas/FormTareas.js
@@ -1,5 +1,6 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import proyectoContext from '../../context/proyectoContext';
+import TareaContext from '../../context/Tareas/tareaContext';
 
 const FormTareas = () => {
 
@@ -7,15 +8,48 @@ const FormTareas = () => {
     const proyectosContext = useContext(proyectoContext);
     const { proyecto } = proyectosContext;
 
+    const tareasContext = useContext(TareaContext);
+    const { agregarTarea } = tareasContext;
+
+    // state del formulario
+    const [tarea, guardarTarea] = useState({
+        nombre: ''
+    })
+    const { nombre } = tarea;
+
     // Si no hay proyecto seleccionado
     if (!proyecto) return null;
+
+    // leer los valores del formulario
+    const handleChange = e => {
+        guardarTarea({
+            ...tarea,
+            [e.target.name]: e.target.value
+        })
+    }
 
     // Array Destructuring para extraer el proyecto actual
     const [proyectoActual] = proyecto;
 
+    const onSubmit = e => {
+        e.preventDefault();
+
+        // validando formulario
+
+        // pasar la validacion
+
+        // agregar la nueva tarea
+        tarea.proyectoId = proyectoActual.id;
+        tarea.estado = false
+        agregarTarea(tarea);
+
+        // reiniciar form
+
+    }
+
     return (
         <div className="formulario">
-            <form action="">
+            <form onSubmit={onSubmit}>
                 <div className="content input">
                     <input
                         type="text"
@@ -23,6 +57,8 @@ const FormTareas = () => {
                         id="nombre"
                         className="input-text"
                         placeholder="Nombre Tarea..."
+                        value={nombre}
+                        onChange={handleChange}
                     />
                 </div>
 
