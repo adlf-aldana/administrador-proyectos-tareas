ca7c2f0d95759993a6ceaf6e5c239f67abfcc307
Adolfo <adlf.aldana@gmail.com>
2020-08-18 14:34:38 -0400

    insertando usuario a la BD y solucionando algunos problemas

--- servidor/controllers/usuarioController.js
+++ servidor/controllers/usuarioController.js
@@ -1,4 +1,20 @@
-exports.crearUsuario = (req, res) => {
-    // res.send('hola');
-    console.log(req.body);
+const Usuario = require('../models/Usuario');
+
+exports.crearUsuario = async (req, res) => {
+    try {
+        let usuario;
+        
+        // Crea el nuevo usuario
+        usuario = new Usuario(req.body);
+
+        // Guardar usuario
+        await usuario.save();
+
+        // Mensaje de confirmacion
+        res.send('Usuario creado correctamente')
+    } catch (error) {
+        console.log(error);
+        res.status(400).send('Hubo un error');
+        
+    }
 }
