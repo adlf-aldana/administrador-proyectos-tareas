66debe728319ab29af0fcdd488e1aa6a48911332
Adolfo <adlf.aldana@gmail.com>
2020-08-19 00:14:40 -0400

    creando el controller de autentificacion

---
+++ servidor/routes/auth.js
@@ -0,0 +1,15 @@
+// Rutas para autenticar usuarios
+const express = require('express');
+const router = express.Router();
+const { check } = require('express-validator');
+const authController = require('../controllers/authController');
+
+router.post('/',
+    [
+        check('email', 'Agregar un email válido').isEmail(),
+        check('password', 'El password debe ser mínimo de 6 caracteres').isLength({ min: 6 })
+    ],
+    authController.autentificarUsuario
+);
+
+module.exports = router;
