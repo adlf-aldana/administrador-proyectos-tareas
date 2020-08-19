cd4fd84431ad75a1e81ab72a37b97dc285b8e348
Adolfo <adlf.aldana@gmail.com>
2020-08-19 01:19:15 -0400

    verificando si el usuario esta autentificado para crear un nuevo proyecto

--- servidor/routes/proyectos.js
+++ servidor/routes/proyectos.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const proyectoController = require('../controllers/proyectoController');
+const auth = require('../midleware/auth');
 
 // Crea proyectos
 // api/proyectos
 router.post('/',
+    auth,
+    proyectoController.crearProyecto
+);
+router.get('/',
+    auth,
     proyectoController.crearProyecto
 );
 
