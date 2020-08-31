3bf1178bf39320fb395b76160a18cbfcc04933d6
adolfo <adlf.aldana@gmail.com>
2020-08-31 19:11:24 -0400

    creando el state de alertas

--- src/App.js
+++ src/App.js
@@ -5,18 +5,21 @@ import NuevaCuenta from './components/auth/NuevaCuenta';
 import Proyectos from './components/proyectos/Proyectos';
 import ProyectoState from './context/proyectoState';
 import TareaState from './context/Tareas/tareaState';
+import AlertaState from './context/alertas/alertaState';
 
 function App() {
   return (
     <ProyectoState>
       <TareaState>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/" component={Login} />
-            <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
-            <Route exact path="/proyectos" component={Proyectos} />
-          </Switch>
-        </BrowserRouter>
+        <AlertaState>
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/" component={Login} />
+              <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
+              <Route exact path="/proyectos" component={Proyectos} />
+            </Switch>
+          </BrowserRouter>
+        </AlertaState>
       </TareaState>
     </ProyectoState>
   );
