51c06f2b8176a71244e73127594c698cb1f07685
adolfo <adlf.aldana@gmail.com>
2020-08-31 19:35:35 -0400

    validando que no haya campos vacios y mostrando alertas y estilos agregados para alertas

--- src/components/auth/NuevaCuenta.js
+++ src/components/auth/NuevaCuenta.js
@@ -1,7 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Link } from 'react-router-dom'
+import AlertaContext from '../../context/alertas/alertaContext';
 
 const NuevaCuenta = () => {
+
+    // extraer los valores del context
+    const alertaContext = useContext(AlertaContext);
+    const { alerta, mostrarAlerta } = alertaContext;
 
     // State para iniciar sesion
     const [usuario, guardarUsuario] = useState({
@@ -25,10 +30,24 @@ const NuevaCuenta = () => {
     const onSubmit = e => {
         e.preventDefault();
 
+        // Validar que no haya campos vacios
+        if (nombre.trim() === '' ||
+            email.trim() === '' ||
+            password.trim() === '' ||
+            confirmar.trim() === '') {
+            mostrarAlerta('Todos los campos son obligatorios', 'alerta-error');
+        }
     }
 
     return (
         <div className="form-usuario">
+            {alerta ?
+                (
+                    <div className={`alerta ${alerta.categoria}`}>
+                        {alerta.msg}
+                    </div>
+                )
+                : null}
             <div className="contenedor-form sombra-dark">
                 <h1>Obtener nueva cuenta</h1>
                 <form
