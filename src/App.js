7dc2e1bc09af03953f39bbf01b99b16c7f9158a4
adolfo <adlf.aldana@gmail.com>
2020-08-31 20:16:33 -0400

    creando state y provider

--- src/App.js
+++ src/App.js
@@ -6,19 +6,22 @@ import Proyectos from './components/proyectos/Proyectos';
 import ProyectoState from './context/proyectoState';
 import TareaState from './context/Tareas/tareaState';
 import AlertaState from './context/alertas/alertaState';
+import AuthState from './context/autentificacion/authState';
 
 function App() {
   return (
     <ProyectoState>
       <TareaState>
         <AlertaState>
-          <BrowserRouter>
-            <Switch>
-              <Route exact path="/" component={Login} />
-              <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
-              <Route exact path="/proyectos" component={Proyectos} />
-            </Switch>
-          </BrowserRouter>
+          <AuthState>
+            <BrowserRouter>
+              <Switch>
+                <Route exact path="/" component={Login} />
+                <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
+                <Route exact path="/proyectos" component={Proyectos} />
+              </Switch>
+            </BrowserRouter>
+          </AuthState>
         </AlertaState>
       </TareaState>
     </ProyectoState>
